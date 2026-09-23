2365f4868ff5c0ff05bfb5846f46e1a9f3eb20b5
Md. Solayman Kabir <solaymankabir997@gmail.com>
2026-09-23 23:23:52 +0600

    Update github_image.py

--- github_image.py
+++ github_image.py
@@ -1,19 +1,16 @@
-import urllib.parse
-
 def get_working_image_url(raw_image_url):
-    """BDStall এর হটলিংক বাইপাস করে নির্ভরযোগ্য CDN ইমেজ প্রস্তুত করা"""
+    """BDStall এর সরাসরি সঠিক ইমেজ লিঙ্ক প্রস্তুত করা"""
     if not raw_image_url:
         return ""
         
-    # ইউআরএল ফরম্যাট সঠিকভাবে ঠিক করা
     raw_image_url = raw_image_url.strip()
+    
+    # ইউআরএল ফরম্যাট সঠিকভাবে ঠিক করা
     if raw_image_url.startswith('//'):
-        raw_image_url = 'https:' + raw_image_url
+        return 'https:' + raw_image_url
     elif raw_image_url.startswith('http://'):
-        raw_image_url = raw_image_url.replace('http://', 'https://')
+        return raw_image_url.replace('http://', 'https://')
     elif not raw_image_url.startswith('http'):
-        raw_image_url = 'https://www.bdstall.com/' + raw_image_url.lstrip('/')
+        return 'https://www.bdstall.com/' + raw_image_url.lstrip('/')
         
-    #wsrv.nl CDN দিয়ে এনকোডেড ইমেজ রিটার্ন
-    encoded_url = urllib.parse.quote(raw_image_url, safe='')
-    return f"https://wsrv.nl/?url={encoded_url}&output=jpg&n=-1"
+    return raw_image_url
